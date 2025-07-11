2c4448c44799aa1da5d28bccee08ee0673732346
heetsolanki <heetsolanki2006@gmail.com>
2025-07-11 18:45:37 +0530

    Create array-size.py

---
+++ array-size.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+array = np.array([1, 7, 13, 105])
+
+print("Size of the array: ", array.size)
+print("Item Size of the array: ", array.itemsize)
+print("Total bytes consumed by the array: ", array.nbytes)
