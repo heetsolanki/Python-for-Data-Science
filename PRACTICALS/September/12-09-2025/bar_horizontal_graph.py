f9f354eeab63c5a3bc73f538d567d045aa394995
Heet Solanki <heetsolanki2006@gmail.com>
2025-09-16 20:27:30 +0530

    Create bar_horizontal_graph.py

--- PRACTICALS/September/12-09-2025/bar_horizontal_graph.py
+++ PRACTICALS/September/12-09-2025/bar_horizontal_graph.py
@@ -6,7 +6,7 @@ df = pd.read_csv("C:\\Users\\admin\\Downloads\\Coffe_sales.csv")
 coffee_types = df["coffee_name"].unique()
 coffee_totals = [df[df["coffee_name"] == c]["money"].sum() for c in coffee_types]
 
-plt.bar(coffee_types, coffee_totals, color="skyblue")
+plt.barh(coffee_types, coffee_totals, color="skyblue")
 plt.title("Total Sales by Coffee Type")
 plt.xlabel("Coffee Type")
 plt.ylabel("Total Sales")
